862e6b652dd2805074ae349181c0758286d7896f
ry0y4n <rusk.ryoy4n@gmail.com>
2020-07-15 03:07:42 +0900

    first: implement soundvisualizer & easy haptic & audio feedback system

---
+++ MicrophoneMonitor.swift
@@ -0,0 +1,77 @@
+// iPhoneからの音声をキャプチャするためのロジックモデルファイル
+
+// ユーザーの携帯電話からのオーディオの'録音'に関連した設定と，サウンドレベルバッファが変更されたときのパブリッシングを扱う
+// AVAudioSessionを設定し，0.01秒ごとにサウンドレベルをキャプチャするように設定
+
+import Foundation
+import AVFoundation
+
+class MicrophoneMonitor: ObservableObject {
+    
+    // オーディオレコーダーとオプションのタイマーを設定
+    private var audioRecorder: AVAudioRecorder
+    private var timer: Timer?
+    
+    private var currentSample: Int
+    private let numberOfSamples: Int
+    
+    // @Publishedをつけることで変更されるたびにUIも更新される
+    @Published public var soundSamples: [Float]
+    
+    init(numberOfSamples: Int) {
+        self.numberOfSamples = numberOfSamples
+        self.soundSamples = [Float](repeating: .zero, count: numberOfSamples)
+        self.currentSample = 0
+        
+        // プライバシーのパーミッションが許可されているかどうかを確認
+        // ユーザーが拒否した場合fatalErrorを投げる
+        let audioSession = AVAudioSession.sharedInstance()
+        if audioSession.recordPermission != .granted {
+            audioSession.requestRecordPermission { (isGranted) in
+                if !isGranted {
+                    fatalError("You must allow audio recording for this demo to work")
+                }
+            }
+        }
+        
+        // オーディオ録音が'一時的に保存'される場所のために私たちのURLを作成
+        let url = URL(fileURLWithPath: "/dev/null", isDirectory: true)
+        let recorderSettings: [String:Any] = [
+            AVFormatIDKey: NSNumber(value: kAudioFormatAppleLossless),
+            AVSampleRateKey: 1,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue
+        ]
+        
+        // audioRecorderを作成．これで録音できるようになった
+        do {
+            audioRecorder = try AVAudioRecorder(url: url, settings: recorderSettings)
+            try audioSession.setCategory(.playAndRecord, mode: .default, options: [])
+            
+            startMonitoring()
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    // このオブジェクトが初期化されるとすぐにモニタリングを開始
+    private func startMonitoring() {
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.record()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { (timer) in
+            
+            // updateMeters() を呼び出すことで、オーディオレコーダーの全チャンネルの平均とピークパワー値を更新
+            // これによりサウンドチャンネルの平均レベルを取得する
+            // またサウンドサンプルを、その時点での audioPower() の値に更新する必要がある
+            self.audioRecorder.updateMeters()
+            self.soundSamples[self.currentSample] = self.audioRecorder.averagePower(forChannel: 0)
+            self.currentSample = (self.currentSample + 1) % self.numberOfSamples
+        })
+    }
+    
+    //　オブジェクトの非初期化
+    deinit {
+        timer?.invalidate()
+        audioRecorder.stop()
+    }
+}
---
+++ SoundVisualizer/ContentView.swift
@@ -0,0 +1,110 @@
+//
+//  ContentView.swift
+//  SoundVisualizer
+//
+//  Created by momosuke on 2020/07/15.
+//  Copyright © 2020 momosuke. All rights reserved.
+//
+
+import SwiftUI
+import CoreHaptics
+
+let numberOfSamples: Int = 10
+
+struct ContentView: View {
+    @State private var engine: CHHapticEngine?
+    // マイクロホンモニターのobservedObjectを作成
+    @ObservedObject private var mic = MicrophoneMonitor(numberOfSamples: numberOfSamples)
+    
+    // 生のサウンドレベル（マイクモニターから使用するために与えられたレベル）を取り込むヘルパー関数を作成
+    // -160 ~ 0の入力を0.1 ~ 25の間に正則化し，可視化の都合で300までにさらに変換
+    private func normalizeSoundLevel(level: Float) -> CGFloat {
+        let level = max(0.2, CGFloat(level) + 50) / 2
+        
+        return CGFloat(level * (300 / 25))
+    }
+    
+    var body: some View {
+        VStack {
+            // 正規化されたバーを表示
+            HStack(spacing: 4) {
+                ForEach(mic.soundSamples, id: \.self) { level in
+                    BarView(value: self.normalizeSoundLevel(level: level))
+                }
+            }
+            Text("Feel the Vibes")
+                .onAppear(perform: prepareHaptics)
+                .onTapGesture(perform: playHaptics)
+        }
+    }
+    
+    func prepareHaptics() {
+
+        // 接続デバイスで触覚フィードバックをサポートしているかチェック
+        guard  CHHapticEngine.capabilitiesForHardware()
+            .supportsHaptics else { print("no support"); return; }
+        do {
+            // 触覚フィードバックスタート
+            self.engine = try! CHHapticEngine()
+            try engine!.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription).")
+        }
+        print("a")
+    }
+    
+    func playHaptics() {
+        self.mic.deinit
+        guard CHHapticEngine.capabilitiesForHardware()
+            .supportsHaptics  else { print("no support"); return; }
+//        var events = [CHHapticEvent]() // CHHapticEvent: 1つの触覚またはオーディオイベントオブジェクト
+//
+//        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+//        events.append(event)
+        
+        let audioEvent = CHHapticEvent(eventType: .audioContinuous, parameters: [
+        CHHapticEventParameter(parameterID: .audioPitch, value: -0.15),
+        CHHapticEventParameter(parameterID: .audioVolume, value: 1),
+        CHHapticEventParameter(parameterID: .decayTime, value: 1),
+        CHHapticEventParameter(parameterID: .sustained, value: 0)
+        ], relativeTime: 0)
+        let hapticEvent = CHHapticEvent(eventType: .hapticContinuous, parameters: [
+        CHHapticEventParameter(parameterID: .hapticSharpness, value: 1),
+        CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        ], relativeTime: 0, duration: 3)
+        
+        do {
+            let pattern = try! CHHapticPattern(events: [audioEvent, hapticEvent], parameters: [])
+            //let pattern = try CHHapticPattern(events: events, parameters: []) // CHHapticPattern: イベントを束ねるオブジェクト
+            let player = try engine?.makePlayer(with: pattern) // makePlayer: パターンからプレイヤーをsakusei
+            try player?.start(atTime: CHHapticTimeImmediate)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+        print("b")
+    }
+}
+
+struct BarView: View {
+
+    // サウンドレベルを UI が認識できるものに変換
+    var value: CGFloat
+    
+    var body: some View {
+        ZStack {
+            // ラウンドとった長方形で可視化
+            RoundedRectangle(cornerRadius: 20)
+                .fill(LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .top, endPoint: .bottom))
+            
+                .frame(width: (UIScreen.main.bounds.width - CGFloat(numberOfSamples) * 4) / CGFloat(numberOfSamples), height: value)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
